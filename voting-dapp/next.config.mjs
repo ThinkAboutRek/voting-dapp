12e048a1dabcb7d12fdf989529dcf0725199b98d
Shayan <sb2018@canterbury.ac.uk>
2025-03-22 05:16:24 +0500

    dapp

--- voting-dapp/next.config.mjs
+++ voting-dapp/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "standalone", 
+    trailingSlash: true,  
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, 
+    },
+  };
+  
+  module.exports = nextConfig;
+  
