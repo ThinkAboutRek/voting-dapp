efa8ecda9631126e08ce945ad0ac1cf5a912c8c9
Shayan <sb2018@canterbury.ac.uk>
2025-03-22 05:23:03 +0500

    dapp

--- voting-dapp/src/app/context/KYCModalContext.tsx
+++ voting-dapp/src/app/context/KYCModalContext.tsx
@@ -47,7 +47,7 @@ export const KYCModalProvider: FC<ModalProviderProps> = ({ children }) => {
     } else if (!account) {
       closeModal();
     }
-  }, [kycAccessToken, account, kycStatus,openModal]);
+  }, [kycAccessToken, account, kycStatus]);
   
 
   const applicantStatus = async (id: string) => {
