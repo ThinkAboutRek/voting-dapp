8446aa652c86f9b809c3896fcdc5e07edb4d33db
Shayan <sb2018@canterbury.ac.uk>
2025-03-22 13:28:56 +0500

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,15 @@ const app = express()
 const port = 5001;
 
 
-app.use(cors())
-
-
-app.use(express.json(),corscors({
+app.use(cors({
   origin: [`http://localhost:${port}`, "https://voting-dapp-inner-bgxl.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-}));
+}))
+
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({message:'Shapater'})
